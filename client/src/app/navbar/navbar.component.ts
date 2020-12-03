092bddff0c0f756089ea922b4c25a8134c58e05b
nathan <nathankfoss@gmail.com>
2020-12-03 12:59:12 -0600

    Create Navbar Component with GET function

--- client/src/app/navbar/navbar.component.ts
+++ client/src/app/navbar/navbar.component.ts
@@ -1,4 +1,8 @@
+import { HttpResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { tap } from 'rxjs/operators';
+import { DataService, User } from '../data.service';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +11,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private dataService: DataService, private router: Router) { }
 
   ngOnInit() {
+  }
+
+  onNavigate(target: string): void {
+    this.dataService.onNavigate(target)
+      .subscribe(
+        (userList: User[]) => {
+          console.log(userList[0].name)
+        },
+        error => {
+          console.log(error)
+        })
+  }
+
+  onPostUser(): void {
+    this.dataService.postUser(this.dataService.user)
   }
 
 }
