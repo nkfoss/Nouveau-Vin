e6329e5f19bf0a6cb1cbcfe485356c0e85060433
nathan <nathankfoss@gmail.com>
2020-12-03 12:58:45 -0600

    Update app module for previous commits

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule } from '@angular/core'
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { ReviewListComponent } from './review-list/review-list.component';
+import { ReviewItemComponent } from './review-list/review-item/review-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    ReviewListComponent,
+    ReviewItemComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
