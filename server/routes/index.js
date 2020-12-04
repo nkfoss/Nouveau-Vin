894e1e0ee9e3253fab4315b30cfb75992920eab8
nathan <nathankfoss@gmail.com>
2020-12-04 10:52:32 -0600

    Add query for browsing countries

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,20 +4,20 @@ var router = express.Router();
 const mysqlDb = require('../db/mysqlConn')
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
+router.get('/countries', function (req, res, next) {
   // res.render('index', { title: 'Express' });
-  const query = 'SELECT * FROM users where id > ?';
+  const query = 'SELECT country, COUNT(*) AS numReviews FROM wineReviews GROUP  BY country';
 
   // Here we have our query, with 3 things:
   //  1) A SQL statement to execute (exluding data from the web), with a '?' placeholder for scrubbed data (see #2)
   //  2) any data from the web stored in AN ARRAY. This allows the data to be scrubbed. IMPORTANT.
   //  3) A callback function to execute containing a possible error, the fetched results, and 'fields
-  mysqlDb.query(query, [3], (error, results) => {
+  mysqlDb.query(query, [], (error, results) => {
     if (error) {
       console.log(error);
       res.send(error);
     } else {
-      console.log("got here")
+      console.log(results)
       res.send(results);
     }
   })
