32aa69ef7037f37bc3f23df00e5ff5bfcc45bbcf
nathan <nathankfoss@gmail.com>
2020-12-04 14:01:37 -0600

    Update express router to have wildcard route for browsing criteria

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,12 +1,21 @@
-const e = require('express');
 var express = require('express');
 var router = express.Router();
 const mysqlDb = require('../db/mysqlConn')
 
 /* GET home page. */
-router.get('/countries', function (req, res, next) {
+router.get('/:browsingCriteria', function (req, res, next) {
   // res.render('index', { title: 'Express' });
-  const query = 'SELECT country, COUNT(*) AS numReviews FROM wineReviews GROUP  BY country';
+  const criteria = req.params.browsingCriteria;
+  let query = '';
+  if(req.params.browsingCriteria === "country") {
+    query = 'SELECT country AS value, COUNT(*) AS numReviews FROM wineReviews GROUP BY country'
+  }
+  else if(req.params.browsingCriteria === "variety") {
+    query = 'SELECT variety AS value, COUNT(*) AS numReviews FROM wineReviews GROUP BY variety'
+  }
+  else if(req.params.browsingCriteria === "critic") {
+    query = 'SELECT taster_name AS value, COUNT(*) AS numReviews FROM wineReviews GROUP BY taster_name'
+  }
 
   // Here we have our query, with 3 things:
   //  1) A SQL statement to execute (exluding data from the web), with a '?' placeholder for scrubbed data (see #2)
@@ -17,7 +26,7 @@ router.get('/countries', function (req, res, next) {
       console.log(error);
       res.send(error);
     } else {
-      console.log(results)
+      // console.log(results)
       res.send(results);
     }
   })
