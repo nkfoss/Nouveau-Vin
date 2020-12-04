71ea3a90dd8b5c410689492b22c7b772751f8be0
nathan <nathankfoss@gmail.com>
2020-12-04 14:52:15 -0600

    Enable browsing list to update without reload when new criteria selected

--- client/src/app/review-list/review-list.component.ts
+++ client/src/app/review-list/review-list.component.ts
@@ -1,3 +1,4 @@
+import { ThrowStmt } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Observable, of } from 'rxjs';
@@ -16,17 +17,30 @@ export class ReviewListComponent implements OnInit {
   constructor(private dataService: DataService, private router: Router, private activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
-    this.browsingCriteria = this.activatedRoute.snapshot.paramMap.get('browsingCriteria')
-    this.dataService.fetchBrowseItems(this.browsingCriteria).subscribe(
-      (fetched: BrowseItem[]) => {
-        console.log("Fetch successful;")
-        this.browseItems = fetched;
-      },
-      (error) => {
-        console.log(error);
-        this.browseItems = [];
-      }
-    )
+    this.activatedRoute.params.subscribe(routeParams => {
+      this.browsingCriteria = routeParams['browsingCriteria'];
+      this.dataService.fetchBrowseItems(this.browsingCriteria).subscribe(
+        (fetched: BrowseItem[]) => {
+          console.log("Fetch successful;")
+          this.browseItems = fetched;
+        },
+        (error) => {
+          console.log(error);
+          this.browseItems = [];
+        }
+      )
+    })
+    // this.activatedRoute.snapshot.paramMap.get('browsingCriteria')
+    // this.dataService.fetchBrowseItems(this.browsingCriteria).subscribe(
+    //   (fetched: BrowseItem[]) => {
+    //     console.log("Fetch successful;")
+    //     this.browseItems = fetched;
+    //   },
+    //   (error) => {
+    //     console.log(error);
+    //     this.browseItems = [];
+    //   }
+    // )
   }
 
 
