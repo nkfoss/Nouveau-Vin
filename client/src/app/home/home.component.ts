0a201dae5e755075660d0d951017da915418daa7
nathan <nathankfoss@gmail.com>
2020-12-05 01:29:36 -0600

    Update Home Component to get fetched reviews

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params, UrlSegment } from '@angular/router';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,65 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  browsingCriteria: string;
+  chosenCriteria: string;
+
+  constructor(private activatedRoute: ActivatedRoute, private dataService: DataService) { }
 
   ngOnInit() {
+    this.activatedRoute.params.subscribe(
+      (params: Params) => {
+
+        if (params['browsingCriteria']) {
+          this.browsingCriteria = params['browsingCriteria'];
+          this.chosenCriteria = params['chosenCriteria'];
+          this.dataService.fetchReviewItems(this.browsingCriteria, this.chosenCriteria).subscribe(
+            (reviews: any) => { console.log(reviews) },
+            (error) => { console.log(error) } 
+          )
+        }
+
+        else {
+          // Load the regular home page.
+          console.log("we are home")
+        }
+      }
+    )
+
+       // console.log("segment change")
+      // this.browsingCriteria = segments[0].path;
+      // if (segments.length === 2) {
+      //   this.chosenCriteria = segments[1].path;
+      // }
+      // this.dataService.fetchReviewItems(this.browsingCriteria, this.chosenCriteria).subscribe(
+      //   (reviews: any) => {
+      //      console.log(reviews)
+      //   },
+      //   (error) => {
+      //     console.log(error)
+      //   } 
+      // )
+    // this.activatedRoute.params.subscribe(routeParams => {
+
+    //   this.browsingCriteria = routeParams['browsingCriteria'];
+    //   if (this.browsingCriteria) {
+
+    //   }
+    // })
+  }
+
+  initSpecificReviews() {
+
+    // this.dataService.fetchReviewItems(this.browsingCriteria).subscribe(
+    //   (fetched: BrowseItem[]) => {
+    //     console.log("Fetch successful;")
+    //     this.browseItems = fetched;
+    //   },
+    //   (error) => {
+    //     console.log(error);
+    //     this.browseItems = [];
+    //   }
+    // )
   }
 
 }
