7129809c055cef82f39f730e219d9d5530a92a9f
nathan <nathankfoss@gmail.com>
2020-12-06 18:11:52 -0600

    Update scripts to display full variety list

--- client/src/app/review-list/review-list.component.ts
+++ client/src/app/review-list/review-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute, Router, UrlSegment } from '@angular/router';
 import { BrowseItem, DataService } from '../data.service';
 
 @Component({
@@ -9,31 +9,44 @@ import { BrowseItem, DataService } from '../data.service';
 })
 export class ReviewListComponent implements OnInit {
 
+  longView = false;
   browsingCriteria: string
   browseItems: BrowseItem[];
 
   constructor(private dataService: DataService, private router: Router, private activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
-    console.log('inited')
-    this.activatedRoute.params.subscribe(routeParams => {
-      this.browsingCriteria = routeParams['browsingCriteria'];
-      this.dataService.fetchBrowseItems(this.browsingCriteria).subscribe(
-        (fetched: BrowseItem[]) => {
-          console.log("Fetch successful;")
-          this.browseItems = fetched;
-        },
-        (error) => {
-          console.log(error);
-          this.browseItems = [];
-        }
-      )
+    this.activatedRoute.url.subscribe((segments: UrlSegment[]) => {
+      this.browsingCriteria = segments[0].path
+
+      if (segments.length === 2) {
+        this.longView = true;
+        this.dataService.fetchAllVarieties().subscribe(
+          (fetched: BrowseItem[]) => {
+            console.log("Fetch successful;")
+            this.browseItems = fetched;
+          }, (error) => {
+            console.log(error);
+            this.browseItems = [];
+          }
+        ) 
+      } else {
+        this.dataService.fetchBrowseItems(this.browsingCriteria).subscribe(
+          (fetched: BrowseItem[]) => {
+            console.log("Fetch successful;")
+            this.browseItems = fetched;
+          }, (error) => {
+            console.log(error);
+            this.browseItems = [];
+          }
+        )
+      }
     })
   }
 
   onChooseCriteria(index: number) {
     let chosenCriteria = this.browseItems[index].value;
-    this.router.navigate([chosenCriteria], {relativeTo: this.activatedRoute})
+    this.router.navigate([chosenCriteria], { relativeTo: this.activatedRoute })
   }
 
 
