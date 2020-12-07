03bcfddc8ec3a1924b58091d39ea591a4eac2946
nathan <nathankfoss@gmail.com>
2020-12-06 18:10:16 -0600

    Add DB query for full variety list

--- server/routes/index.js
+++ server/routes/index.js
@@ -27,19 +27,32 @@ router.get('/:browsingCriteria', function (req, res, next) {
   })
 });
 
+router.get('/variety/all', function(req,res,next) {
+  query = "SELECT variety as value FROM varieties"
+  mysqlDb.query(query, [], (error, results) => {
+    if (error) {
+      console.log(error);
+      res.send(error);
+    } else {
+      // console.log(results)
+      res.send(results);
+    }
+  })
+})
+
 router.get('/:browsingCriteria/:selectedCriteria', function(req, res, next) {
   const browsingCriteria = req.params.browsingCriteria;
   const selectedCriteria = req.params.selectedCriteria;
   let query = '';
   if (browsingCriteria === 'country') {
     console.log('it is country')
-    query = 'SELECT * FROM wineReviews WHERE country = ?'
+    query = 'select * from wineReviews as w join countries as c on w.fkCountry = c.id where c.country = ?'
   }
   else if (browsingCriteria === 'variety') {
-    query = 'SELECT * FROM wineReviews WHERE variety = ?'
+    query = 'SELECT * FROM winereviews AS w JOIN varieties AS v ON w.fkVariety = v.id WHERE v.variety = ?'
   }
   else if (browsingCriteria === 'critic') {
-    query = 'SELECT * FROM winereviews WHERE taster_name = ?'
+    query = 'SELECT * FROM winereviews AS w JOIN tasters AS t ON w.fkTaster = t.id WHERE t.taster_name = ?'
   }
 
   mysqlDb.query(query, [selectedCriteria], (error, results) => {
