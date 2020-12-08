e5e675cede795d4f5bbf01f0cab5ba6c0b5b022a
nathan <nathankfoss@gmail.com>
2020-12-07 21:22:20 -0600

    Add QOL changes

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 const mysqlDb = require('../db/mysqlConn');
 
 router.get('/countReviews', function (req, res, next) {
-  let query = 'select count(*) as count from winereviews';
+  let query = 'SELECT count(*) AS count FROM winereviews';
   mysqlDb.query(query, [], (error, results) => {
     if (error) { console.log(error); res.send(error); }
     else {
@@ -29,28 +29,20 @@ function getRandomsQuery(numRows) {
   }
   idList = idList.replace(/,[ ]$/, ')')
   let joinedTables =
-    'select * from wineReviews as w ' +
-    'join countries as c on w.fkCountry = c.id ' +
-    'join varieties as v on w.fkVariety = v.id ' +
-    'join tasters as t on w.fkTaster = t.id ';
-  let query = joinedTables + 'WHERE w.id in ' + idList;
+    'SELECT * FROM wineReviews AS w ' +
+    'JOIN countries AS c ON w.fkCountry = c.id ' +
+    'JOIN varieties AS v ON w.fkVariety = v.id ' +
+    'JOIN tasters AS t ON w.fkTaster = t.id ';
+  let query = joinedTables + 'WHERE w.id IN ' + idList;
   console.log(query);
   return query;
 }
 
-/* GET home page. */
 router.get('/:browsingCriteria', function (req, res, next) {
-  const criteria = req.params.browsingCriteria;
   let query = '';
-  if (req.params.browsingCriteria === "country") {
-    query = 'SELECT country AS value, numReviews FROM countries';
-  }
-  else if (req.params.browsingCriteria === "variety") {
-    query = `SELECT variety AS value, numReviews FROM varieties WHERE numReviews > 1000 ORDER BY value`;
-  }
-  else if (req.params.browsingCriteria === "critic") {
-    query = 'SELECT taster_name AS value, numReviews FROM tasters';
-  }
+  if (req.params.browsingCriteria === "country") { query = 'SELECT country AS value, numReviews FROM countries'; }
+  else if (req.params.browsingCriteria === "variety") { query = `SELECT variety AS value, numReviews FROM varieties WHERE numReviews > 1000 ORDER BY value`; }
+  else if (req.params.browsingCriteria === "critic") { query = 'SELECT taster_name AS value, numReviews FROM tasters'; }
 
   mysqlDb.query(query, [], (error, results) => {
     if (error) { console.log(error); res.send(error); }
@@ -67,20 +59,17 @@ router.get('/variety/all', function (req, res, next) {
 })
 
 router.get('/:browsingCriteria/:selectedCriteria', function (req, res, next) {
-  const browsingCriteria = req.params.browsingCriteria;
-  const selectedCriteria = req.params.selectedCriteria;
   let query = '';
   let joinedTables =
     'select * from wineReviews as w ' +
     'join countries as c on w.fkCountry = c.id ' +
     'join varieties as v on w.fkVariety = v.id ' +
     'join tasters as t on w.fkTaster = t.id ';
+  if (req.params.browsingCriteria === 'country') { query = joinedTables + 'WHERE c.country = ?'; }
+  else if (req.params.browsingCriteria === 'variety') { query = joinedTables + 'WHERE v.variety = ?'; }
+  else if (req.params.browsingCriteria === 'critic') { query = joinedTables + 'WHERE t.taster_name = ?'; }
 
-  if (browsingCriteria === 'country') { query = joinedTables + 'WHERE c.country = ?'; }
-  else if (browsingCriteria === 'variety') { query = joinedTables + 'WHERE v.variety = ?'; }
-  else if (browsingCriteria === 'critic') { query = joinedTables + 'WHERE t.taster_name = ?'; }
-
-  mysqlDb.query(query, [selectedCriteria], (error, results) => {
+  mysqlDb.query(query, [req.params.selectedCriteria], (error, results) => {
     if (error) { console.log(error); res.send(error); }
     else { res.send(results); }
   })
