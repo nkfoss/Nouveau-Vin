043bfa05172c9f867383c6a3672ba9eb70fac7d0
nathan <nathankfoss@gmail.com>
2020-12-08 18:26:01 -0600

    Put review/browse items in their own files

--- client/src/app/review-list/review-list.component.ts
+++ client/src/app/review-list/review-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router, UrlSegment } from '@angular/router';
-import { BrowseItem, DataService } from '../data.service';
+import { DataService } from '../data.service';
+import { BrowseItem } from '../shared/browseitem.model';
 
 @Component({
   selector: 'app-review-list',
