ed7662deecc3913ea80f7ee27619300947b41fc9
nathan <nathankfoss@gmail.com>
2020-12-10 11:56:15 -0600

    Remove unused  component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -8,7 +8,6 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { ReviewListComponent } from './review-list/review-list.component';
 import { ReviewItemComponent } from './review-list/review-item/review-item.component';
 import { HomeComponent } from './home/home.component';
-import { CategoryItemComponent } from './review-list/category-item/category-item.component';
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
@@ -17,7 +16,6 @@ import { FormsModule } from '@angular/forms';
     NavbarComponent,
     ReviewListComponent,
     ReviewItemComponent,
-    CategoryItemComponent,
     HomeComponent
   ],
   imports: [
