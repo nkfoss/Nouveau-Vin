031d203f5d45e231da82d42785378d6476cdafad
nathan <nathankfoss@gmail.com>
2020-12-13 11:03:55 -0600

    Differentiate between angular and database routes

--- server/app.js
+++ server/app.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const router = require('./routes/index');
 const bodyParser = require('body-parser');
 const express = require('express');
@@ -5,6 +6,7 @@ const express = require('express');
 const app = express();
 
 app.use(bodyParser.json());
+app.use("/", express.static(path.join(__dirname, "build")))
 
 app.use((req,res,next) => {
 	res.setHeader('Access-Control-Allow-Origin', '*');  // Incoming requests can come from '*' aka ANYWHERE
@@ -12,10 +14,14 @@ app.use((req,res,next) => {
 	res.setHeader('Acces-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, DELETE');
 	next();
 })
-app.use('/', router);
 
-app.use((req,res,next) => {
-	res.send('Sent.')
-})
+
+app.use('/wine/api/', router);
+app.use('/', (req,res,next) => {
+	res.sendFile(path.join(__dirname, "build", "index.html"))
+});
+
+
+
 
 module.exports = app; // This exports the app AND all the middlewares
