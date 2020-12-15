0ea063936a57f73620bf92a62fe10709bf31d416
nathan <nathankfoss@gmail.com>
2020-12-15 11:38:48 -0600

    Update App Module

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core'
 import { HttpClientModule } from '@angular/common/http'
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,7 +9,7 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { ReviewListComponent } from './review-list/review-list.component';
 import { ReviewItemComponent } from './review-list/review-item/review-item.component';
 import { HomeComponent } from './home/home.component';
-import { FormsModule } from '@angular/forms';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { FormsModule } from '@angular/forms';
     NavbarComponent,
     ReviewListComponent,
     ReviewItemComponent,
-    HomeComponent
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
