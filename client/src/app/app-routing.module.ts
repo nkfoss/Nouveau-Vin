7da3911b28e74df57c59e39ea22632bc687ccaa9
nathan <nathankfoss@gmail.com>
2020-12-15 11:38:33 -0600

    Create Angular route for login

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 import { ReviewListComponent } from './review-list/review-list.component';
 
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
   {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: 'login', component: LoginComponent},
   {path: 'variety/all', component: ReviewListComponent},
   
   {path: ':browsingCriteria', component: ReviewListComponent},
